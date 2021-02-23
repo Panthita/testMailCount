7bfec00a3c76f68313d3e97aadb1d5a649094b39
AmpPanthita <45354251+Panthita@users.noreply.github.com>
2021-02-23 10:47:42 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    control.reset()
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString("Hello!")
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    control.reset()
+})
+let Mail = 0
+radio.setGroup(117)
+basic.forever(function () {
+    if (tinkercademy.PIR(DigitalPin.P0)) {
+        Mail += 1
+        basic.showNumber(Mail)
+        basic.pause(500)
+        radio.sendNumber(Mail)
+    } else {
+        basic.showNumber(Mail)
+        basic.pause(100)
+    }
+})
